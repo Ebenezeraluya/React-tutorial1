bfa6aaac8f59d9cec0efa2b1c97d92be23c7a77a
unknown <ebenezeraluya@gmail.com>
2022-08-09 15:49:09 +0100

    This is the initial commit

---
+++ src/Components/Body/Body.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Body.css'
+
+
+function Body() {
+  return (
+    <div id='fle1'>
+        <div>
+        <h1 className='remote'>Make <br></br>remote work</h1>
+        </div>
+        <h3 className='team'>
+         Get your team in sync, no matter your location. <br></br>
+         Streamline processes, create team rituals, and <br></br>
+         watch productivity soar.
+         </h3> <br></br>
+         <button id='button'>Learn More</button>
+    </div>
+  )
+}
+
+export default Body;
---
+++ src/Components/Navbar/Nav.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Nav.css'
+
+
+function Nav() {
+  return (
+    <div id='fle'>
+        <div>
+        <h2 className='snap'>snap</h2>
+        </div>
+        <nav >
+            <ul id='nav1'>
+                <li>Features &darr;</li>
+                <li>Company &darr;</li>
+                <li>Careers</li>
+                <li>About</li>
+            </ul>
+        </nav>
+    </div>
+  )
+}
+
+export default Nav;
